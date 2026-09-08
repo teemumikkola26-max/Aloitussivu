f3fc8c73d05134b0e2d549dffa1bb7efb7dd4862
teemumikkola26-max <teemu.mikkola26@gmail.com>
2026-09-08 14:50:12 +0300

    Update auth-gate.js

--- auth-gate.js
+++ auth-gate.js
@@ -20,7 +20,7 @@
   "use strict";
 
   // ---- TÄYTÄ NÄMÄ OMAN SUPABASE-PROJEKTISI TIEDOILLA ----
-  const SUPABASE_URL = "https://xuaoqdpmhhpxjcweaauz.supabase.co";
+  const SUPABASE_URL = "https://xuaoqdpmhhpxjcweaauz.supabase.co/rest/v1/";
   const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1YW9xZHBtaGhweGpjd2VhYXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1ODg3NzksImV4cCI6MjEwNDE2NDc3OX0.vU_vlkS6WpLQoTaVt5gZTsbsSSJZg-GVnpcCiJxRivw";
   // --------------------------------------------------------
 
